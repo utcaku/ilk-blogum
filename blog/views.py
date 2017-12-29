33ce54d116392b716a839f5cb0741b90e6eb9bf0
utcaku <kaplanutcaku@hotmail.com>
2017-12-29 15:27:41 +0300

    Viewlerdeki hatayı düzelttim.

--- blog/views.py
+++ blog/views.py
@@ -9,11 +9,11 @@ def post_list(request):
     posts = Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-	
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-	
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -22,11 +22,11 @@ def post_new(request):
             post.yazar = request.user
             post.yayinlanma_tarihi = timezone.now()
             post.save()
-            return redirect('blog.views.post_detail', pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-	
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -36,7 +36,7 @@ def post_edit(request, pk):
             post.yazar = request.user
             post.yayinlanma_tarihi = timezone.now()
             post.save()
-            return redirect('blog.views.post_detail', pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
